9cbd082d75f2c96198259b28b6bef1356ce22507
john <john@jpatmore.plus.com>
2021-01-05 23:25:13 +0000

    Completed code

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+import quiz_brain as qb
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface():
-    def __init__(self):
+    def __init__(self, quiz: qb.QuizBrain):
+        self.quiz = quiz
+
         self.window = tk.Tk()
         self.window.title("Quizzler")
         self.window.config(
@@ -23,11 +26,11 @@ class QuizInterface():
             width=300,
             height=250)
 
-        # Set tuple to half-width, half-height for centered text
         self.text_question = self.canvas.create_text(
-            (150, 125),
+            (150, 125),  # Set tuple to half-width, half-height for centered text
             text="Question",
-            fill="black",
+            width=280,  # Set text width for wrapping
+            fill=THEME_COLOR,
             font=("Arial", 20, "italic"))
 
         image_false = tk.PhotoImage(file="./images/false.png")
@@ -36,7 +39,8 @@ class QuizInterface():
             width=100,
             height=97,
             bd=0,
-            highlightthickness=0)
+            highlightthickness=0,
+            command=self.check_answer_false)
 
         image_true = tk.PhotoImage(file="./images/true.png")
         self.button_true = tk.Button(
@@ -44,7 +48,8 @@ class QuizInterface():
             width=100,
             height=97,
             bd=0,
-            highlightthickness=0)
+            highlightthickness=0,
+            command=self.check_answer_true)
 
         # Grid Layout
         self.label_score.grid(row=0, column=1)
@@ -52,15 +57,31 @@ class QuizInterface():
         self.button_false.grid(row=2, column=0)
         self.button_true.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
 
-    def next_question(self):
-        pass
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            question = self.quiz.next_question()
+            self.canvas.itemconfig(self.text_question, text=question)
+        else:
+            question = "Completed"
+            self.canvas.itemconfig(self.text_question, text=question)
+            self.button_false.config(state=tk.DISABLED)
+            self.button_true.config(state=tk.DISABLED)
 
-        # self.canvas.itemconfig(self.text_question, text=question)
+    def check_answer_false(self):
+        self.feedback(self.quiz.check_answer("False"))
 
-    def check_answer(self):
-        pass
+    def check_answer_true(self):
+        self.feedback(self.quiz.check_answer("True"))
 
-        # self.score = 0
-        # self.score_text = str(self.score)
+    def feedback(self, correct):
+        if correct:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.label_score.config(text=f"Score: {self.quiz.score}/{self.quiz.question_number}")
+        self.window.after(1500, self.get_next_question)  # No parentheses in func!!
